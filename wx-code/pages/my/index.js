bef6d844af67987ec896f8153dba53946dba6820
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-18 10:44:28 +0800

    Update index.js

--- wx-code/pages/my/index.js
+++ wx-code/pages/my/index.js
@@ -1,66 +1,47 @@
-// pages/my/index.js
+// pages/index.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    visibleMessage: false,
+    currAuthStep: "3",
+    userInfo: {
+      avatarUrl: 'https://we-retail-static-1300977798.cos.ap-guangzhou.myqcloud.com/retail-ui/components-exp/avatar/avatar-1.jpg',
+      nickName: '热情仔',
+      phoneNumber: '13438358888',
+      gender: 2,
     },
+  },
+  onWithdraw() {
+    this.setData({
+      visibleMessage: true
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+  },
 
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    this.getTabBar().init();
+    wx.showShareMenu({
+      withShareTicket: true,
+      menus: ['shareAppMessage', 'shareTimeline']
+    })
+  },
+  onShareTimeline(e) {
+    console.log(e, 1111)
+    return {
+      userName: '小程序原始id',
+      path: 'pages/index/index',
+      title: '标题',
     }
+  }
 })
