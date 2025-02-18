2197fb34b12a998d3a505a755760943cb4becbd0
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-18 17:08:16 +0800

    Create index.js

---
+++ wx-code/pages/my/withdrawal-record/index.js
@@ -0,0 +1,77 @@
+// pages/my/withdrawal-record/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    recordList: [{
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      },
+      {
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      },
+      {
+        title: "提现50元 审核中",
+        status: "process",
+        desc: " 2024-10-11 12:00:12"
+      }, {
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      }, {
+        title: "提现10元 提现失败",
+        status: "error",
+        desc: "不符合提现条件 2024-10-14 12:00:12"
+      },{
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      },
+      {
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      },
+      {
+        title: "提现50元 审核中",
+        status: "process",
+        desc: " 2024-10-11 12:00:12"
+      }, {
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      }, {
+        title: "提现10元 提现失败",
+        status: "error",
+        desc: "不符合提现条件 2024-10-14 12:00:12"
+      },{
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      },
+      {
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      },
+      {
+        title: "提现50元 审核中",
+        status: "process",
+        desc: " 2024-10-11 12:00:12"
+      }, {
+        title: "提现20元 已成功",
+        status: "finish",
+        desc: "2024-10-13 12:00:12"
+      }, {
+        title: "提现10元 提现失败",
+        status: "error",
+        desc: "不符合提现条件 2024-10-14 12:00:12"
+      }
+    ]
+  },
+})
