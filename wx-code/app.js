35fc15fc165ae322354f6ab296b471a78e77ef79
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-18 14:57:02 +0800

    Update app.js

--- wx-code/app.js
+++ wx-code/app.js
@@ -3,5 +3,9 @@ import updateManager from './common/updateManager';
 App({
   onShow() {
     updateManager();
+    wx.showShareMenu({
+      withShareTicket: true,
+      menus: ['shareAppMessage', 'shareTimeline']
+    })
   },
 });
