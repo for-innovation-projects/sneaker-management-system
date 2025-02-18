10fde39770406d31a9b91011640e1df0259c08de
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-18 10:38:53 +0800

    Create data.js

---
+++ wx-code/custom-tab-bar/data.js
@@ -0,0 +1,16 @@
+export default [{
+    icon: 'home',
+    "pagePath": "pages/index/index",
+    "text": "首页"
+  },
+  {
+    icon: 'cart',
+    "pagePath": "pages/order/index",
+    "text": "订单"
+  },
+  {
+    icon: 'person',
+    "pagePath": "pages/my/index",
+    "text": "我的"
+  }
+];
