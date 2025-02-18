f024bbf674ef1a64c2b572436811fdbac8e3a4f1
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-18 10:39:32 +0800

    Add files via upload

---
+++ wx-code/custom-tab-bar/index.js
@@ -0,0 +1,29 @@
+import TabMenu from './data';
+Component({
+  data: {
+    active: 0,
+    list: TabMenu,
+  },
+
+  methods: {
+    onChange(event) {
+      this.setData({
+        active: event.detail.value
+      });
+      wx.switchTab({
+        url: `/${this.data.list[event.detail.value].pagePath}`,
+      });
+    },
+
+    init() {
+      const page = getCurrentPages().pop();
+      const route = page ? page.route.split('?')[0] : '';
+      const active = this.data.list.findIndex(
+        (item) => item.pagePath === route,
+      );
+      this.setData({
+        active
+      });
+    },
+  },
+});
