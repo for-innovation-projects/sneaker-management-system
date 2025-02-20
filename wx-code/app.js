c57a1d1d54123c510eb427fbcd624765ce9a78b8
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-20 15:57:17 +0800

    Update app.js

--- wx-code/app.js
+++ wx-code/app.js
@@ -1,6 +1,12 @@
 import updateManager from './common/updateManager';
 
+import {
+  getScreenInfo
+} from './utils/jsapi'
 App({
+  onLaunch() {
+    getScreenInfo();
+  },
   onShow() {
     updateManager();
     wx.showShareMenu({
