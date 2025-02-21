07aeac175c934b76af968210af608af5add1ef1a
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-21 16:50:28 +0800

    Update index.js

--- wx-code/pages/order/components/shop-item/index.js
+++ wx-code/pages/order/components/shop-item/index.js
@@ -1,10 +1,14 @@
 // pages/order/components/shop-item/index..js
+import {
+  ORDER_STATUS
+} from "../../../../common/index"
 Component({
 
   /**
    * 组件的属性列表
    */
   properties: {
+    status: "",
     selectStatus: false,
     checked: false,
     value: ""
@@ -14,7 +18,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    ORDER_STATUS
   },
 
   /**
