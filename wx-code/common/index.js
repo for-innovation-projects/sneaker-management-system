17f3413b3672e1d24dfe9aabdcb4a00979a62298
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-21 16:49:16 +0800

    Create index.js

---
+++ wx-code/common/index.js
@@ -0,0 +1,12 @@
+export const ORDER_STATUS = {
+  /** 当前订单 */
+  curOrder: 'curOrder',
+  /** 审核中 */
+  process: 'process',
+  /** 待确认 */
+  beConfirmed: 'beConfirmed',
+  /** 退货订单 */
+  returnGood: 'returnGood',
+  /** 已完成 */
+  finish: 'finish',
+}
