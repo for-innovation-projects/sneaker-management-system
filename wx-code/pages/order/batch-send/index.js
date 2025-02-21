00f31d2d6c47e9df8517e3fcea59def230655045
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-21 11:04:59 +0800

    Create index.js

---
+++ wx-code/pages/order/batch-send/index.js
@@ -0,0 +1,47 @@
+// pages/order/batch-send/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    number: 0,
+    shopList: [{
+      value: "aa"
+    }, {
+      value: "aa1"
+    }, {
+      value: "aa2"
+    }, {
+      value: "aa3"
+    }, {
+      value: "aa4"
+    }],
+    checkedList: []
+  },
+  onChange(e) {
+    const {
+      checked,
+      value
+    } = e.detail
+    let newCheckedList = [...this.data.checkedList]
+    if (checked) {
+      newCheckedList.push(value);
+    } else {
+      newCheckedList = newCheckedList.filter(item => item !== value)
+    }
+    newCheckedList = [...new Set(newCheckedList)]
+    this.setData({
+      checkedList: newCheckedList
+    })
+  },
+  onChangeAll(e) {
+    let newCheckedList = []
+    if (e.detail.checked) {
+      newCheckedList = this.data.shopList.map(item => item.value)
+    }
+    this.setData({
+      checkedList: newCheckedList
+    })
+  }
+})
