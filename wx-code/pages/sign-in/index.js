d97ba25d162a3542d2c0d2a0075948513b20598d
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-28 10:18:44 +0800

    Create index.js

---
+++ wx-code/pages/sign-in/index.js
@@ -0,0 +1,52 @@
+// pages/sign-in/index.js
+import {
+  innerPhoneReg,
+} from '../../utils/index'
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    formData: {
+      phone: "",
+      code: ""
+    }
+  },
+  onInputValue(e) {
+    const {
+      item
+    } = e.currentTarget.dataset;
+    const {
+      value = ''
+    } = e.detail;
+    this.setData({
+      [`formData.${item}`]: value,
+    });
+  },
+  handleLogin() {
+    const {
+      phone,
+      code
+    } = this.data.formData
+    let text = '';
+    if (!phone) {
+      text = '请输入手机号'
+    } else if (!innerPhoneReg.test(phone)) {
+      text = '请填写正确的手机号'
+    }
+    wx.switchTab({
+      url: '/pages/index/index',
+    })
+    if (text) {
+      wx.showToast({
+        title: text,
+        icon: "none"
+      })
+    }
+  },
+  handleRegister() {
+    wx.navigateTo({
+      url: '/pages/register/index',
+    })
+  }
+})
