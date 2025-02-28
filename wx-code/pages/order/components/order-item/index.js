c8385d9826f2513af79eeeff61bba8a49a9832f7
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-28 10:14:33 +0800

    Create index.js

---
+++ wx-code/pages/order/components/order-item/index.js
@@ -0,0 +1,28 @@
+// pages/order/components/order-item/index.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    status: ''
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onShopList() {
+      wx.navigateTo({
+        url: '/pages/order/batch-send/index?orderStatus=' + this.properties.status,
+      })
+    }
+  }
+})
