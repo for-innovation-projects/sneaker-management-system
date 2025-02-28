46a7b6f3e6828b1dbb77d7ad06abecba17ac1764
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-28 16:05:23 +0800

    Update index.tsx

--- web-code/src/pages/Address/index.tsx
+++ web-code/src/pages/Address/index.tsx
@@ -1,6 +1,6 @@
 import type { ProColumns } from '@ant-design/pro-components';
 import { ProTable } from '@ant-design/pro-components';
-import { Button, Input, Modal } from 'antd';
+import { Button, Form, Input, Modal } from 'antd';
 import { useState } from 'react';
 
 const { TextArea } = Input;
@@ -8,6 +8,8 @@ const { TextArea } = Input;
 export type TableListItem = {
   key: number;
   date: number;
+  name: string;
+  phone: string;
   address: string;
 };
 const tableListDataSource: TableListItem[] = [];
@@ -16,6 +18,8 @@ for (let i = 0; i < 5; i += 1) {
   tableListDataSource.push({
     key: i,
     date: Date.now(),
+    name: '倪明晨',
+    phone: '17345899634',
     address: i % 2 === 1 ? '黑龙江省伊春市伊春区新立小区' : '简短备注文案',
   });
 }
@@ -26,6 +30,14 @@ export default () => {
   const [addressInfo, setAddressInfo] = useState('');
 
   const columns: ProColumns<TableListItem>[] = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+    },
+    {
+      title: '手机号',
+      dataIndex: 'phone',
+    },
     {
       title: '地址信息',
       dataIndex: 'address',
@@ -91,7 +103,38 @@ export default () => {
         onOk={() => {}}
         onCancel={() => setIsModalOpen(false)}
       >
-        <TextArea value={addressInfo} rows={4} />
+        <Form
+          name="basic"
+          labelCol={{ span: 5 }}
+          wrapperCol={{ span: 19 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="姓名"
+            name="name"
+            rules={[{ required: true, message: '请输入姓名' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="手机号"
+            name="phone"
+            rules={[{ required: true, message: '请输入手机号' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="地址"
+            name="address"
+            rules={[{ required: true, message: '请输入地址信息' }]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+        </Form>
       </Modal>
     </>
   );
