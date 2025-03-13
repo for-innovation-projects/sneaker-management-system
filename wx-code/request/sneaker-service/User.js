94cd6af03054e96ff5958e9f0e7253208ebd1950
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-13 11:22:22 +0800

    Create User.js

---
+++ wx-code/request/sneaker-service/User.js
@@ -0,0 +1,68 @@
+import {
+  request
+} from "../index";
+export function wechat_login_api_wechatuser_login_post({
+  data,
+} = {}) {
+  return request({
+    url: `/api/wechatuser/login`,
+    method: 'post',
+    data: data || {},
+  });
+}
+export function wechat_authentication_api_wechatuser_authentication_post({
+  data,
+}) {
+  return request({
+    url: `/api/wechatuser/authentication`,
+    method: 'post',
+    data: data || {},
+  }, );
+}
+export function wechat_authentication_api_wechatuser_receive_post({
+  data,
+}) {
+  return request({
+    url: `/api/wechatuser/receive`,
+    method: 'post',
+    data: data || {},
+  }, );
+}
+export function wechat_authentication_api_wechatuser_receive_patch({
+  data,
+}) {
+  return request({
+    url: `/api/wechatuser/receive`,
+    method: 'patch',
+
+    data: data || {},
+  });
+}
+export function wechat_authentication_api_wechatuser_information_get({
+  data,
+}) {
+  return request({
+    url: `/api/wechatuser/information`,
+    method: 'get',
+
+    data: data || {},
+  }, );
+}
+export function send_verification_code_api_wechatuser_send_verification_code_post({
+  data,
+}) {
+  return request({
+    url: `/api/wechatuser/send-verification-code`,
+    method: 'post',
+    data: data || {},
+  }, );
+}
+export function verify_code_api_wechatuser_verify_code_post({
+  data,
+}) {
+  return request({
+    url: `/api/wechatuser/verify-code`,
+    method: 'post',
+    data: data || {},
+  });
+}
