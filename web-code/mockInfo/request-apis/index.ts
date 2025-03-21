dd648137f2cc645a13604d69be2a40009c7988b2
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-21 15:52:05 +0800

    Delete web-code/mockInfo/request-apis directory

--- web-code/mockInfo/request-apis/index.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosRequestConfig } from 'axios';
-const request = axios.create({
-  baseURL: '',
-});
-const customFetch = <T>(config: AxiosRequestConfig): Promise<T> => {
-  return request(config) as any;
-};
-export default customFetch;
--- web-code/mockInfo/request-apis/sneaker-service/Upload.ts
+++
@@ -1,7 +0,0 @@
-import request from "../index";
-export function upload_file_api_upload__post() {
-  return request<IApi.UploadUploadFileApiUpload_PostResponses>({
-    url: `/api/upload/`,
-    method: "post",
-  });
-}
