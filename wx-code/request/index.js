75134739e863fd88473d861f0a2a184cf28e77b6
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-24 15:36:14 +0800

    Update index.js

--- wx-code/request/index.js
+++ wx-code/request/index.js
@@ -1,13 +1,20 @@
+import {
+  getOpenId
+} from "../utils/store"
 export const request = (params = {}) => {
   const {
     url,
     method,
     data
   } = params
+  const result = getOpenId() || {}
   return new Promise((resolve, reject) => {
     wx.request({
-      url: "http://kmexhj.natappfree.cc" + url,
-      data,
+      url: "http://89ipri.natappfree.cc" + url,
+      data: {
+        ...result,
+        ...data
+      },
       method,
       success(res) {
         resolve(res)
