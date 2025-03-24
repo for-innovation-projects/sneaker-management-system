76df63bd503fd45a2c6c22c3e02cd9b197c5b534
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-24 15:08:49 +0800

    Update customUpload.ts

--- web-code/src/request-apis/customUpload.ts
+++ web-code/src/request-apis/customUpload.ts
@@ -3,7 +3,7 @@ export function upload_file_api_banner_pc_upload_post(
   data: FormData,
   options: any,
 ) {
-  return request<IApi.UploadUploadFileApiBannerPcUploadPostResponses>({
+  return request<IApi.BannerUploadFileApiWechatbannerPcUploadPostResponses>({
     url: `/api/wechatbanner/pc/upload`,
     method: 'post',
     data,
