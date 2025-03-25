992cb1973d9b21e2f137bf17cd11e98e574a5749
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-25 14:12:22 +0800

    Update index.tsx

--- web-code/src/pages/OrderList/index.tsx
+++ web-code/src/pages/OrderList/index.tsx
@@ -197,7 +197,7 @@ export default () => {
         columns={columns}
         request={(params, sorter, filter) => {
           // 表单搜索项会从 params 传入，传递给后端接口。
-          console.log(params, sorter, filter);
+          console.log(params, sorter, filter, tableListDataSource);
           return Promise.resolve({
             data: tableListDataSource,
             success: true,
