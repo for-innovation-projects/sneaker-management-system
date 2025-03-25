07cfcc8521666c3acb7cd97a675e70b98994b5d7
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-25 17:20:15 +0800

    Update index.tsx

--- web-code/src/pages/UserList/index.tsx
+++ web-code/src/pages/UserList/index.tsx
@@ -1,57 +1,43 @@
+import {
+  ban_wechat_user_api_wechatuser_pc_wechat_delete,
+  get_users_api_wechatuser_pc_wechat_get,
+  modify_balance_api_wechatuser_pc_wechat_patch,
+  unban_wechat_user_api_wechatuser_pc_wechat_post,
+} from '@/request-apis/sneaker-service/User';
 import { CopyOutlined } from '@ant-design/icons';
-import type { ProColumns } from '@ant-design/pro-components';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable } from '@ant-design/pro-components';
-import { Input, InputNumber, message, Modal, Timeline } from 'antd';
-import { useState } from 'react';
+import { Input, InputNumber, message, Modal, Popconfirm, Timeline } from 'antd';
+import { useRef, useState } from 'react';
 
 const { TextArea } = Input;
-
-export type TableListItem = {
-  key: number;
-  name: string;
-  idCard: string;
-  phone: string;
-  remainingMoney: number;
-  status: number;
-  date: number;
-};
-const tableListDataSource: TableListItem[] = [];
-
-for (let i = 0; i < 5; i += 1) {
-  tableListDataSource.push({
-    key: i,
-    name: '姓名' + i,
-    idCard: '230702189888889644',
-    phone: '17349867759',
-    remainingMoney: 20 + i,
-    status: i % 2,
-    date: Date.now(),
-  });
-}
 
 export default () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalOpenMoney, setIsModalOpenMoney] = useState(false);
-  const [modalRemainingMoney, setModalRemainingMoney] = useState(0);
+  const [modalRemainingMoney, setModalRemainingMoney] = useState<
+    Partial<IApi.WeChatUserInformationOut>
+  >({});
+  const actionRef = useRef<ActionType>(null);
 
-  const columns: ProColumns<TableListItem>[] = [
+  const columns: ProColumns<IApi.WeChatUserInformationOut>[] = [
     {
       title: '姓名',
       dataIndex: 'name',
     },
     {
       title: '身份证号',
-      dataIndex: 'idCard',
+      dataIndex: 'id_code',
       copyable: true,
       render(_, item) {
         return (
           <>
-            {item.idCard.replace(/^(.{6})(?:\d+)(.{4})$/, '$1********$2')}
+            {item.id_code.replace(/^(.{6})(?:\d+)(.{4})$/, '$1********$2')}
             <CopyOutlined
               style={{ marginLeft: 4, cursor: 'pointer', color: '#1677ff' }}
               onClick={(e) => {
                 e.stopPropagation();
-                navigator.clipboard.writeText(item.idCard).then(() => {
+                navigator.clipboard.writeText(item.id_code).then(() => {
                   message.success('复制成功');
                 });
               }}
@@ -67,12 +53,12 @@ export default () => {
     },
     {
       title: '剩余金额',
-      dataIndex: 'remainingMoney',
+      dataIndex: 'balance',
       search: false,
     },
     {
       title: '账号状态',
-      dataIndex: 'status',
+      dataIndex: 'is_deleted',
       valueType: 'select',
       valueEnum: {
         0: { text: '正常', status: 'Success' },
@@ -84,7 +70,7 @@ export default () => {
     },
     {
       title: '创建时间',
-      dataIndex: 'date',
+      dataIndex: 'create_time',
       valueType: 'dateTime',
     },
     {
@@ -93,7 +79,49 @@ export default () => {
       key: 'option',
       valueType: 'option',
       render: (_, item) => [
-        <a key="link2">{item.status < 1 ? '封禁' : '解封'}</a>,
+        <Popconfirm
+          title="确定要封禁/解封用户么"
+          description="封禁后该用户将无法进入小程序?"
+          onConfirm={() => {
+            if (item.is_deleted === 0) {
+              ban_wechat_user_api_wechatuser_pc_wechat_delete({
+                params: {
+                  id: item.id,
+                },
+              })
+                .then((res) => {
+                  // @ts-ignore
+                  if (res.code === 1) {
+                    message.success('已成功封禁');
+                    actionRef.current?.reload();
+                  }
+                })
+                .catch(() => {
+                  message.error('请重新尝试');
+                });
+            } else {
+              unban_wechat_user_api_wechatuser_pc_wechat_post({
+                params: {
+                  id: item.id,
+                },
+              })
+                .then((res) => {
+                  // @ts-ignore
+                  if (res.code === 1) {
+                    actionRef.current?.reload();
+                    message.success('已成功解除封禁');
+                  }
+                })
+                .catch(() => {
+                  message.error('请重新尝试');
+                });
+            }
+          }}
+          okText="确定"
+          cancelText="取消"
+        >
+          <a key="link2">{item.is_deleted === 0 ? '封禁' : '解封'}</a>
+        </Popconfirm>,
         <a key="link3" onClick={() => setIsModalOpen(true)}>
           提现记录
         </a>,
@@ -101,7 +129,7 @@ export default () => {
           key="link"
           onClick={() => {
             setIsModalOpenMoney(true);
-            setModalRemainingMoney(item.remainingMoney);
+            setModalRemainingMoney({ ...item });
           }}
         >
           修改余额
@@ -111,17 +139,41 @@ export default () => {
   ];
   return (
     <>
-      <ProTable<TableListItem>
+      <ProTable<IApi.WeChatUserInformationOut>
+        actionRef={actionRef}
         columns={columns}
-        request={(params, sorter, filter) => {
+        request={(params) => {
           // 表单搜索项会从 params 传入，传递给后端接口。
-          console.log(params, sorter, filter);
-          return Promise.resolve({
-            data: tableListDataSource,
-            success: true,
+          return new Promise((resolve) => {
+            const { pageSize, current, ...rest } = params;
+            get_users_api_wechatuser_pc_wechat_get({
+              params: {
+                page: current,
+                page_size: pageSize,
+                ...rest,
+              },
+            })
+              .then((res) => {
+                //@ts-ignore
+                if (res.code === 1) {
+                  resolve({
+                    data: (res.data as any) || [],
+                    success: true,
+                  });
+                } else {
+                  resolve({
+                    success: false,
+                  });
+                }
+              })
+              .catch(() => {
+                resolve({
+                  success: false,
+                });
+              });
           });
         }}
-        rowKey="key"
+        rowKey="id"
         dateFormatter="string"
         search={{
           collapsed: false,
@@ -130,13 +182,40 @@ export default () => {
       <Modal
         title="修改余额"
         open={isModalOpenMoney}
-        onOk={() => {}}
+        onOk={() => {
+          const { id, balance } = modalRemainingMoney;
+          modify_balance_api_wechatuser_pc_wechat_patch({
+            params: {
+              balance: String(balance),
+              id,
+            },
+          })
+            .then((res) => {
+              // @ts-ignore
+              if (res.code === 1) {
+                actionRef.current?.reload();
+                message.success('修改成功');
+                setIsModalOpenMoney(false);
+                setModalRemainingMoney({});
+              }
+            })
+            .catch(() => {
+              message.error('请重新尝试');
+            });
+        }}
         onCancel={() => setIsModalOpenMoney(false)}
       >
         <InputNumber
+          precision={2}
           style={{ width: '100%' }}
-          value={modalRemainingMoney}
-          onChange={(value) => setModalRemainingMoney(value ?? 0)}
+          value={modalRemainingMoney.balance}
+          step="0.01"
+          onChange={(value) =>
+            setModalRemainingMoney({
+              ...modalRemainingMoney,
+              balance: value || '',
+            })
+          }
         />
       </Modal>
       <Modal
