facae10420630d01f29cd1273bb5d8c88cca1c32
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-31 16:51:32 +0800

    Update index.ts

--- web-code/src/request-apis/index.ts
+++ web-code/src/request-apis/index.ts
@@ -3,7 +3,7 @@ import { history } from '@umijs/max';
 import { message } from 'antd';
 import axios, { AxiosRequestConfig } from 'axios';
 const request = axios.create({
-  baseURL: 'http://5arbvb.natappfree.cc/',
+  baseURL: 'http://jaqeqh.natappfree.cc/',
 });
 request.interceptors.request.use(
   function (config) {
