dfc100a68575d52855bca36668bfb82a772b5447
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-03-31 16:51:21 +0800

    Create Withdrawal.ts

---
+++ web-code/src/request-apis/sneaker-service/Withdrawal.ts
@@ -0,0 +1,30 @@
+import request from "../index";
+export function get_withdrawal_api_wechatwithdrawal_pc_withdrawal_get({
+  params,
+}: {
+  params?: IApi.WithdrawalGetWithdrawalApiWechatwithdrawalPcWithdrawalGetParams;
+} = {}) {
+  return request<IApi.WithdrawalGetWithdrawalApiWechatwithdrawalPcWithdrawalGetResponses>(
+    {
+      url: `/api/wechatwithdrawal/pc/withdrawal`,
+      method: "get",
+      params: params || {},
+    },
+  );
+}
+export function get_withdrawal_api_wechatwithdrawal_pc_withdrawal__user_id__get(
+  user_id: string,
+  {
+    params,
+  }: {
+    params?: IApi.WithdrawalGetWithdrawalApiWechatwithdrawalPcWithdrawal_UserId_GetParams;
+  } = {},
+) {
+  return request<IApi.WithdrawalGetWithdrawalApiWechatwithdrawalPcWithdrawal_UserId_GetResponses>(
+    {
+      url: `/api/wechatwithdrawal/pc/withdrawal/${user_id}`,
+      method: "get",
+      params: params || {},
+    },
+  );
+}
